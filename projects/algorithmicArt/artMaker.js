f36b64191069dc4082730967818d188262dcf7ab
Tony Nardi <anthonygt@gmail.com>
2018-08-10 07:14:33 -0400

    more art

--- projects/algorithmicArt/artMaker.js
+++ projects/algorithmicArt/artMaker.js
@@ -1,44 +1,107 @@
-function drawCircle(x, y, radius) {
-  var context = document.getElementById("canvas").getContext("2d");
-  context.fillStyle = "#fff";
-  context.strokeStyle = "#000";
-  context.beginPath();
-  context.arc(x, y, radius, 0, Math.PI * 2);
-  context.fill();
-  context.stroke();
-}
-
-function clear(color) {
-  var context = document.getElementById("canvas").getContext("2d");
-  if (color) {
-    context.fillStyle = color;
-    context.fillRect(0, 0, canvas.width, canvas.height);
-  } else {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+var artMaker = (function() {
+  function drawCircle(x, y, radius) {
+    var context = document.getElementById("canvas").getContext("2d");
+    context.fillStyle = "#fff";
+    context.strokeStyle = "#000";
+    context.beginPath();
+    context.arc(x, y, radius, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
   }
-}
 
-function initCanvas() {
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  function clear(color) {
+    var context = document.getElementById("canvas").getContext("2d");
+    if (color) {
+      context.fillStyle = color;
+      context.fillRect(0, 0, canvas.width, canvas.height);
+    } else {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+  }
 
-  context.fillStyle = "pink";
-  context.fillRect(0, 0, canvas.width, canvas.height);
-}
+  function initCanvas() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-function drawRandomRec() {
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
-  var x = Math.random() * canvas.width;
-  var y = Math.random() * canvas.height;
-  var w = Math.random() * 200;
-  var h = Math.random() * 200;
-  var r = Math.floor(Math.random() * 256);
-  var g = Math.floor(Math.random() * 256);
-  var b = Math.floor(Math.random() * 256);
+    context.fillStyle = "pink";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
 
-  context.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-  context.fillRect(x, y, w, h);
-}
+  function drawRandomRec() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var x = Math.random() * canvas.width;
+    var y = Math.random() * canvas.height;
+    var w = Math.random() * 200;
+    var h = Math.random() * 200;
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    context.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+    context.fillRect(x, y, w, h);
+  }
+
+  function drawSierpinski(depth) {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    clear();
+    context.save();
+    context.translate(canvas.width * 0.5, canvas.height * 0.6);
+    context.scale(0.5 * canvas.width, 0.5 * canvas.height);
+    drawTriangle(depth);
+    context.restore();
+  }
+
+  function drawTriangle(depth) {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    context.fillStyle = "#000";
+    var angle = -Math.PI / 2;
+    if (depth === 0) {
+      context.beginPath();
+
+      context.moveTo(Math.cos(angle), Math.sin(angle));
+
+      angle += (Math.PI * 2) / 3;
+
+      context.lineTo(Math.cos(angle), Math.sin(angle));
+
+      angle += (Math.PI * 2) / 3;
+
+      context.lineTo(Math.cos(angle), Math.sin(angle));
+
+      context.fill();
+    } else {
+      context.save();
+      context.translate(Math.cos(angle) * 0.5, Math.sin(angle) * 0.5);
+      context.scale(0.5, 0.5);
+      drawTriangle(depth - 1);
+      context.restore();
+
+      angle += (Math.PI * 2) / 3;
+      context.save();
+      context.translate(Math.cos(angle) * 0.5, Math.sin(angle) * 0.5);
+      context.scale(0.5, 0.5);
+      drawTriangle(depth - 1);
+      context.restore();
+
+      angle += (Math.PI * 2) / 3;
+      context.save();
+      context.translate(Math.cos(angle) * 0.5, Math.sin(angle) * 0.5);
+      context.scale(0.5, 0.5);
+      drawTriangle(depth - 1);
+      context.restore();
+    }
+  }
+  return {
+    drawCircle: drawCircle,
+    clear: clear,
+    drawRandomRec: drawRandomRec,
+    initCanvas: initCanvas,
+    drawTriangle: drawTriangle,
+    drawSierpinski: drawSierpinski
+  };
+})();
--- projects/algorithmicArt/program1.js
+++ projects/algorithmicArt/program1.js
@@ -1,82 +1,44 @@
 function startApp() {
-  initCanvas();
+  artMaker.initCanvas();
   // drawSomeCircles();
   // drawSomeRects();
   var depth = 0;
 
   document.body.addEventListener("click", function() {
     console.log(depth);
-    drawSierpinski(depth);
+    artMaker.drawSierpinski(depth);
     depth++;
   });
+
+  // drawCirclesWithLoop();
+  drawRandomRecWithLoop();
 }
 
 function drawSomeCircles() {
-  drawCircle(500, 400, 100);
-  drawCircle(300, 300, 250);
-  drawCircle(200, 200, 50);
-  drawCircle(600, 250, 20);
+  artMaker.drawCircle(500, 400, 100);
+  artMaker.drawCircle(300, 300, 250);
+  artMaker.drawCircle(200, 200, 50);
+  artMaker.drawCircle(600, 250, 20);
 }
 
 function drawSomeRects() {
-  drawRandomRec();
-  drawRandomRec();
-  drawRandomRec();
-  drawRandomRec();
-  drawRandomRec();
-  drawRandomRec();
+  artMaker.drawRandomRec();
+  artMaker.drawRandomRec();
+  artMaker.drawRandomRec();
+  artMaker.drawRandomRec();
+  artMaker.drawRandomRec();
+  artMaker.drawRandomRec();
 }
 
-function drawSierpinski(depth) {
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
-  clear();
-  context.save();
-  context.translate(canvas.width * 0.5, canvas.height * 0.6);
-  context.scale(0.5 * canvas.width, 0.5 * canvas.height);
-  drawTriangle(depth);
-  context.restore();
+function drawCirclesWithLoop() {
+  for (var i = 0; i < 1000; i++) {
+    artMaker.drawCircle(i * 2, 400, i / 2);
+  }
 }
 
-function drawTriangle(depth) {
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
-  context.fillStyle = "#000";
-  var angle = -Math.PI / 2;
-  if (depth === 0) {
-    context.beginPath();
-
-    context.moveTo(Math.cos(angle), Math.sin(angle));
-
-    angle += (Math.PI * 2) / 3;
-
-    context.lineTo(Math.cos(angle), Math.sin(angle));
-
-    angle += (Math.PI * 2) / 3;
-
-    context.lineTo(Math.cos(angle), Math.sin(angle));
-
-    context.fill();
-  } else {
-    context.save();
-    context.translate(Math.cos(angle) * 0.5, Math.sin(angle) * 0.5);
-    context.scale(0.5, 0.5);
-    drawTriangle(depth - 1);
-    context.restore();
-
-    angle += (Math.PI * 2) / 3;
-    context.save();
-    context.translate(Math.cos(angle) * 0.5, Math.sin(angle) * 0.5);
-    context.scale(0.5, 0.5);
-    drawTriangle(depth - 1);
-    context.restore();
-
-    angle += (Math.PI * 2) / 3;
-    context.save();
-    context.translate(Math.cos(angle) * 0.5, Math.sin(angle) * 0.5);
-    context.scale(0.5, 0.5);
-    drawTriangle(depth - 1);
-    context.restore();
+function drawRandomRecWithLoop() {
+  for (var i = 0; i < 1000; i++) {
+    artMaker.drawRandomRec();
   }
 }
 
